0f9d47edff2e73ab8c8130095e65f360bdbb735b
javalikescript <javalikescript@free.fr>
2020-05-03 21:49:34 +0200

    Update lua-webview module

--- lua-webview.mk
+++ lua-webview.mk
@@ -1,16 +1,21 @@
 CC ?= gcc
 
 LIBEXT ?= dll
-LIBNAME=webview
-TARGET=$(LIBNAME).$(LIBEXT)
-
-TARGETS=$(TARGET) WebView2Win32.$(LIBEXT)
+LIBNAME = webview
+TARGET = $(LIBNAME).$(LIBEXT)
 
 LUA_PATH = lua
 LUA_LIB = lua53
 
-WEBVIEW_C=webview-c
-MS_WEBVIEW2=$(WEBVIEW_C)/ms.webview2.0.8.355
+WEBVIEW_C = webview-c
+MS_WEBVIEW2 = $(WEBVIEW_C)/ms.webview2.0.8.355
+
+GCC_NAME ?= $(shell $(CROSS_PREFIX)gcc -dumpmachine)
+
+ARCH = x64
+ifeq (,$(findstring x86_64,$(GCC_NAME)))
+  ARCH = x86
+endif
 
 LIBOPT_dll = -O \
   -shared \
@@ -53,14 +58,16 @@ SOURCES = webview.c
 
 OBJS = webview.o
 
-lib: $(TARGETS)
+SRCS = $(WEBVIEW_C)/webview.h $(WEBVIEW_C)/webview-cocoa.c $(WEBVIEW_C)/webview-gtk.c $(WEBVIEW_C)/webview-win32.c
 
-$(TARGET): $(OBJS)
+lib: $(TARGET) WebView2Win32.$(LIBEXT)
+
+$(TARGET): $(OBJS) $(SRCS)
 	$(CC) $(OBJS) $(LIBOPT) -o $(TARGET)
 
 WebView2Win32.so:
 
-WebView2Win32.dll:
+WebView2Win32.dll: $(WEBVIEW_C)/WebView2Win32.h $(WEBVIEW_C)/WebView2Win32.c
 	$(CC) $(WEBVIEW_C)/WebView2Win32.c \
     -shared \
     -static-libgcc \
