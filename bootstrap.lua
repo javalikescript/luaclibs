9ecd8f3928739a3ae8affab5483ae3b6f9d0b564
javalikescript <javalikescript@free.fr>
2023-10-28 15:31:04 +0200

    Enhance luastatic

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,4 @@
-local function exists(path, fail)
+local function exists(path)
   local f, err = io.open(path)
   if f then
     io.close(f)
@@ -9,23 +9,20 @@ end
 
 -- Before running any code, lua collects all command-line arguments in a global table called arg.
 -- The script name goes to index 0, the first argument after the script name goes to index 1, and so on.
-local binName = arg[0]
-arg[0] = nil
-binName = string.gsub(binName, '%.exe$', '')
-for i, v in ipairs(arg) do
-  arg[i - 1] = v
-end
+arg[-1], arg[0] = arg[0], arg[1]
+table.remove(arg, 1)
 
+local binName = string.gsub(arg[-1], '%.exe$', '')
 local scriptName = arg[0]
 if scriptName == '-h' or scriptName == '--help' then
   print('try: '..tostring(binName)..' [luascript [arguments ...]]')
-  print('with "--" or no Lua script, the Lua script with the executable name, "'..binName..'.lua", will be loaded.')
+  print('with "-" or no Lua script, the Lua script with the executable name, "'..binName..'.lua", will be loaded.')
   os.exit(22)
 elseif scriptName == '-v' or scriptName == '--version' then
   print(_VERSION)
   print('luv', require('luv').version_string())
   os.exit(0)
-elseif not scriptName or scriptName == '--' or scriptName == '' then
+elseif not scriptName or scriptName == '-' or scriptName == '' then
   scriptName = binName..'.lua'
   arg[0] = scriptName
 end
